a203972b24bf4efc73d83950b1d2e1b42cdd2ed1
jlegacy-ai <jamalmuhammad.bsaif20@iba-suk.edu.pk>
2024-03-01 00:15:34 +0500

    Models config & hyperParams Add

--- api/src/ml/algos/algos.py
+++ api/src/ml/algos/algos.py
@@ -1,4 +1,4 @@
-from api.src.ml.utils.schemas import LinearRegressionParams
+from api.src.ml.utils.schemas import LinearRegressionParams, RidgeRegressionParams, SGDRegressorParams
 
 algos_collection = {
     "linear_regression": {
@@ -6,6 +6,55 @@ algos_collection = {
         "title": "Linear Regression",
         "description": "Linear regression is a linear approach to modelling the relationship between a scalar response and one or more explanatory variables.",
         "schema": LinearRegressionParams.model_json_schema(),
+        "function": "linear_regression",
+    },
+    "ridge_regression": {
+        "id": 2,
+        "title": "Ridge Regression",
+        "description": "Ridge Regression is a popular linear regression technique that aims to mitigate the issue of multicollinearity (high correlation between predictor variables) and overfitting in predictive modeling",
+        "schema": RidgeRegressionParams.model_json_schema(),
+        "function": "linear_regression",
+    },
+    "sgd_regression": {
+        "id": 3,
+        "title": "Stochastic Gradient Descent Regression",
+        "description": "The SGDRegressor in scikit-learn is a linear model fitted by minimizing a regularized empirical loss with stochastic gradient descent, versatile for handling a variety of regression tasks.",
+        "schema": SGDRegressorParams.model_json_schema(),
+        "function": "linear_regression",
+    },
+    "elastic_net_regression": {
+        "id": 4,
+        "title": "Elastic Net Regression",
+        "description": "The SGDRegressor in scikit-learn is a linear model fitted by minimizing a regularized empirical loss with stochastic gradient descent, versatile for handling a variety of regression tasks.",
+        "schema": SGDRegressorParams.model_json_schema(),
+        "function": "linear_regression",
+    },
+    "lars_regression": {
+        "id": 5,
+        "title": "Lars Regression",
+        "description": "The SGDRegressor in scikit-learn is a linear model fitted by minimizing a regularized empirical loss with stochastic gradient descent, versatile for handling a variety of regression tasks.",
+        "schema": SGDRegressorParams.model_json_schema(),
+        "function": "linear_regression",
+    },
+    "lasso_regression": {
+        "id": 6,
+        "title": "Lasso Regression",
+        "description": "The SGDRegressor in scikit-learn is a linear model fitted by minimizing a regularized empirical loss with stochastic gradient descent, versatile for handling a variety of regression tasks.",
+        "schema": SGDRegressorParams.model_json_schema(),
+        "function": "linear_regression",
+    },
+    "lasso_lars_regression": {
+        "id": 7,
+        "title": "LassoLars Regression",
+        "description": "The SGDRegressor in scikit-learn is a linear model fitted by minimizing a regularized empirical loss with stochastic gradient descent, versatile for handling a variety of regression tasks.",
+        "schema": SGDRegressorParams.model_json_schema(),
+        "function": "linear_regression",
+    },
+    "ard_regression": {
+        "id": 8,
+        "title": "ARD Regression",
+        "description": "The SGDRegressor in scikit-learn is a linear model fitted by minimizing a regularized empirical loss with stochastic gradient descent, versatile for handling a variety of regression tasks.",
+        "schema": SGDRegressorParams.model_json_schema(),
         "function": "linear_regression",
     },
 }
--- api/src/ml/utils/schemas.py
+++ api/src/ml/utils/schemas.py
@@ -50,21 +50,162 @@ class RegMetrics(BaseModel):
     smape: float = Field(...,
                          description="Symmetric Mean Absolute Percentage Error")
 
-
-class LinearRegressionHyperParams(BaseModel):
-    fit_intercept: bool = Field(
-        default=True, description="Whether to calculate the intercept for this model")
-
-
 class RegressionParams(BaseModel):
     config: ConfigParam | None = None
     hyper_params: dict | None = None
+
+# Linear Regression Model Configuation
+# Hyperparameters & Config
+class LinearRegressionHyperParams(BaseModel):
+    fit_intercept: bool = Field(
+        default=True, description="Whether to calculate the intercept for this model")
 
 
 class LinearRegressionParams(BaseModel):
     config: ConfigParam
     hyper_params: LinearRegressionHyperParams
 
+# Ridge Regression Model Configuation
+# Hyperparameters & Config
+class RidgeRegressionHyperParams(BaseModel):
+    alpha: float = Field(
+        default=1.0, description="Constant that multiplies the L2 term, controlling regularization strength.")
+    fit_intercept: bool = Field(
+        default=True, description="Whether to calculate the intercept for this model.")
+    tol: float = Field(
+        default=1e-4, description="Precision of the solution.")
+    solver: str = Field(
+        default='auto', description="Solver to use in the computational routines. Options include 'auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga'. 'auto' chooses the solver based on the type of data.")
+    random_state: int = Field(
+        default=None, description="Used when the solver is 'sag' or 'saga' to shuffle the data.", nullable=True)
+
+
+class RidgeRegressionParams(BaseModel):
+    config: ConfigParam
+    hyper_params: RidgeRegressionHyperParams
+
+
+# Stochastic Gradient Descent Model Configuation
+# Hyperparameters & Config
+class SGDRegressorHyperParams(BaseModel):
+    loss: str = Field(
+        default='squared_error', description="The loss function to be used. Defaults to 'squared_error'. Other options include 'huber', 'epsilon_insensitive', or 'squared_epsilon_insensitive'.")
+    penalty: str = Field(
+        default='l2', description="The penalty (aka regularization term) to be used. Defaults to 'l2'. Other options include 'l1', 'elasticnet', and 'none'.")
+    alpha: float = Field(
+        default=0.0001, description="Constant that multiplies the regularization term. Defaults to 0.0001.")
+    l1_ratio: float = Field(
+        default=0.15, description="The Elastic Net mixing parameter, with 0 <= l1_ratio <= 1. Defaults to 0.15.")
+    max_iter: int = Field(
+        default=1000, description="The maximum number of passes over the training data (aka epochs). Defaults to 1000.")
+    tol: float = Field(
+        default=1e-3, description="The stopping criterion. If it is not None, the iterations will stop when (loss > previous_loss - tol). Defaults to 1e-3.")
+    learning_rate: str = Field(
+        default='invscaling', description="The learning rate schedule. Options include 'constant', 'optimal', 'invscaling', and 'adaptive'. Defaults to 'invscaling'.")
+    eta0: float = Field(
+        default=0.01, description="The initial learning rate for the 'constant', 'invscaling' or 'adaptive' schedules. Defaults to 0.01.")
+
+class SGDRegressorParams(BaseModel):
+    config: ConfigParam
+    hyper_params: SGDRegressorHyperParams
+
+
+# Elastic Net Model Configuation
+# Hyperparameters & Config
+class ElasticNetHyperParams(BaseModel):
+    alpha: float = Field(
+        default=1.0, description="Constant that multiplies the penalty terms. Defaults to 1.0.")
+    l1_ratio: float = Field(
+        default=0.5, description="The Elastic Net mixing parameter, with 0 < l1_ratio <= 1. Defaults to 0.5.")
+    fit_intercept: bool = Field(
+        default=True, description="Whether to calculate the intercept for this model. Defaults to True.")
+    normalize: bool = Field(
+        default=False, description="If True, the regressors X will be normalized before regression. Defaults to False.")
+    max_iter: int = Field(
+        default=1000, description="The maximum number of iterations. Defaults to 1000.")
+    tol: float = Field(
+        default=1e-4, description="The tolerance for the optimization. Defaults to 1e-4.")
+
+class ElasticNetParams(BaseModel):
+    config: ConfigParam
+    hyper_params: ElasticNetHyperParams
+
+
+# Least Angle Regression Model Configuation
+# Hyperparameters & Config
+class LarsHyperParams(BaseModel):
+    fit_intercept: bool = Field(
+        default=True, description="Whether to calculate the intercept for this model. Defaults to True.")
+    normalize: bool = Field(
+        default=True, description="If True, the regressors X will be normalized before regression. Defaults to True.")
+    precompute: bool = Field(
+        default=False, description="Whether to use a precomputed Gram matrix to speed up calculations. Defaults to False.")
+    n_nonzero_coefs: int = Field(
+        default=500, description="The maximum number of non-zero coefficients. Defaults to 500.")
+    eps: float = Field(
+        default=2.220446049250313e-16, description="The machine precision for floating-point operations. Defaults to approximately 2.22e-16.")
+
+class LarsParams(BaseModel):
+    config: ConfigParam
+    hyper_params: LarsHyperParams
+
+# Lasso Regression Model Configuation
+# Hyperparameters & Config
+class LassoHyperParams(BaseModel):
+    alpha: float = Field(
+        default=1.0, description="Constant that multiplies the L1 term. Defaults to 1.0.")
+    fit_intercept: bool = Field(
+        default=True, description="Whether to calculate the intercept for this model. Defaults to True.")
+    normalize: bool = Field(
+        default=False, description="If True, the regressors X will be normalized before regression. Defaults to False.")
+    max_iter: int = Field(
+        default=1000, description="The maximum number of iterations. Defaults to 1000.")
+    tol: float = Field(
+        default=0.0001, description="The tolerance for the optimization. Defaults to 1e-4.")
+
+class LassoParams(BaseModel):
+    config: ConfigParam
+    hyper_params: LassoHyperParams
+
+# LassoLars Regression Model Configuation
+# Hyperparameters & Config
+class LassoLarsHyperParams(BaseModel):
+    alpha: float = Field(
+        default=1.0, description="Constant that multiplies the L1 term. Defaults to 1.0 for LassoLars.")
+    fit_intercept: bool = Field(
+        default=True, description="Whether to calculate the intercept for this model. Defaults to True for LassoLars.")
+    normalize: bool = Field(
+        default=True, description="If True, the regressors X will be normalized before regression. Defaults to True for LassoLars.")
+    max_iter: int = Field(
+        default=500, description="The maximum number of iterations. Adjusted to 500 for LassoLars.")
+    eps: float = Field(
+        default=2.220446049250313e-16, description="The machine precision for floating-point operations. Defaults to approximately 2.22e-16 for LassoLars.")
+
+class LassoLarsParams(BaseModel):
+    config: ConfigParam
+    hyper_params: LassoLarsHyperParams
+
+# ARD Regression Model Configuation
+# Hyperparameters & Config
+class ARDRegressionHyperParams(BaseModel):
+    n_iter: int = Field(
+        default=300, description="Maximum number of iterations. Defaults to 300.")
+    tol: float = Field(
+        default=1e-3, description="Stopping criterion. Defaults to 1e-3.")
+    alpha_1: float = Field(
+        default=1e-6, description="Hyper-parameter : shape parameter for the Gamma distribution prior over the alpha parameter. Defaults to 1e-6.")
+    alpha_2: float = Field(
+        default=1e-6, description="Hyper-parameter : inverse scale parameter (rate parameter) for the Gamma distribution prior over the alpha parameter. Defaults to 1e-6.")
+    lambda_1: float = Field(
+        default=1e-6, description="Hyper-parameter : shape parameter for the Gamma distribution prior over the lambda parameter. Defaults to 1e-6.")
+    lambda_2: float = Field(
+        default=1e-6, description="Hyper-parameter : inverse scale parameter (rate parameter) for the Gamma distribution prior over the lambda parameter. Defaults to 1e-6.")
+    compute_score: bool = Field(
+        default=False, description="If True, compute the objective function at each step of the model. Defaults to False.")
+
+class ARDRegressionParams(BaseModel):
+    config: ConfigParam
+    hyper_params: ARDRegressionHyperParams
 
 class AlgoOutput(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
@@ -75,6 +216,7 @@ class AlgoOutput(BaseModel):
 class AlgoRouteResponseData(BaseModel):
     metrics: RegMetrics
     predictions: dict
+
 class AlgoRouteResponse(BaseModel):
     data: AlgoRouteResponseData
     message: str
